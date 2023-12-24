420fdf7f70e11412f4d848d2801c6353d54a8b6f
raulcarvalhomelo <raulcarvalhomelo@gmail.com>
2023-12-23 23:51:04 -0300

    main

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -29,7 +29,7 @@ export default function Login () {
           <Image className="w-8 hover:opacity-80" width={30} height={30} src={profileimage[1]} alt="GoogleLogin" />
         </button>
         <div className="flex">
-          <button className="flex-none w-14 ">{loginDark==='dark'?(<FcNoIdea onClick={()=>setLoginDark('ligth')}/>):(<FcIdea onClick={()=>setLoginDark('dark')}/>)}</button>
+          <button className="flex-none w-14 ">{loginDark==='dark'?(<FcNoIdea onClick={()=>setLoginDark('ligth')}/>):(<FcIdea onClick={()=>setLoginDark('dark')}/>)}dark</button>
           <p className="grid justify-items-end  -full text-xs">
           {modo==='login'?(
               <p>Novo por aqui?
